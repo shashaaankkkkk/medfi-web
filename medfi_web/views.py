f8c095f9f3909b09a06fa0566c5fe5ded27b00e2
shashaaankkkkk <shashankshekhar8534@gmail.com>
2024-02-24 02:36:26 +0530

    added

--- medfi_web/views.py
+++ medfi_web/views.py
@@ -1,5 +1,42 @@
 from django.shortcuts import render
+from django.contrib.auth import authenticate 
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+
+
+from django.contrib.sites.shortcuts import get_current_site
+
+from django.contrib.auth import authenticate, login, logout
+
+
 
 # Create your views here.
 def index(request):
-    return render(request,"index.html")
+    return render(request,"login.html")
+
+def signin(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        pass1 = request.POST['pass1']
+        
+        user = authenticate(username=username, password=pass1)
+        
+        if user is not None:
+            login(request, user)
+            fname = user.first_name
+            # messages.success(request, "Logged In Sucessfully!!")
+            return render(request, "authentication/index.html",{"fname":fname})
+        else:
+            messages.error(request, "Bad Credentials!!")
+            return redirect('home')
+    
+    return render(request, "authentication/signin.html")
+
+
+def signout(request):
+    logout(request)
+    messages.success(request, "Logged Out Successfully!!")
+    return redirect('home')
