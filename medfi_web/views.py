51758d44e3e7d8efff9e533fb73c20c76fb00a4b
shashaaankkkkk <shashankshekhar8534@gmail.com>
2024-02-25 22:42:01 +0530

    added

--- medfi_web/views.py
+++ medfi_web/views.py
@@ -16,7 +16,7 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 def index(request):
-    return render(request, "login.html")
+    return render(request, "index.html")
 
 def signin(request):
     if request.method == 'POST':
